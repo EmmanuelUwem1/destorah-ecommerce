513f1a028805c5851f4e0cec24ada6bea88cf9c6
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-05-15 23:58:19 +0100

    Add mock product data and handle error responses with mock data

--- lib/products.ts
+++ lib/products.ts
@@ -17,11 +17,140 @@ type ProductData = {
   imageUrl: string;
   extra?: Record<string, any>;
   id: string;
-  benefits: Benefits;
+  benefits?: Benefits;
 };
 
 
-
+const MockProducts: ProductData[] = [
+  {
+    name: "Cassava Paste",
+    description: `<span>
+        Freshly processed, sifted for a smooth texture
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "1",
+    category: "Grain & Staple",
+    imageUrl: "/productCassava 1.png",
+  },
+  {
+    name: "Palm Oil",
+    description: `<span>
+        Freshly processed
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "2",
+    category: "Oils and Condiments",
+    imageUrl: "/Palm-Oil-1L 1.png",
+  },
+  {
+    name: "Cassava Paste",
+    description: `<span>
+        Freshly processed, sifted for a smooth texture
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "3",
+    category: "Grain & Staple",
+    imageUrl: "/fufu.png",
+  },
+  {
+    name: "Palm Oil",
+    description: `<span>
+        Freshly processed
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "4",
+    category: "Oils and Condiments",
+    imageUrl: "/palm oil 2.png",
+  },
+  {
+    name: "Cassava Paste",
+    description: `<span>
+        Freshly processed, sifted for a smooth texture
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "5",
+    category: "Grain & Staple",
+    imageUrl: "/productCassava 1.png",
+  },
+  {
+    name: "Palm Oil",
+    description: `<span>
+        Freshly processed
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "6",
+    category: "Oils and Condiments",
+    imageUrl: "/Palm-Oil-1L 1.png",
+  },
+  {
+    name: "Cassava Paste",
+    description: `<span>
+        Freshly processed, sifted for a smooth texture
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "7",
+    category: "Grain & Staple",
+    imageUrl: "/fufu.png",
+  },
+  {
+    name: "Palm Oil",
+    description: `<span>
+        Freshly processed
+      <ul>
+      <li> 100 % organic: no Additives or preservatives </li>
+      <li> straight from our Nigerian farm to your table </li>
+      <li> Rich taste , authentic traditional taste for every meal. </li>
+      </ul >
+  </span>`,
+    price: "10000",
+    stock: "10",
+    id: "8",
+    category: "Oils and Condiments",
+    imageUrl: "/palm oil 2.png",
+  },
+];
 export async function getProducts(
   storeId: string
 ): Promise<{ success: boolean; products?: ProductData[]; message?: string }> {
@@ -48,11 +177,16 @@ export async function getProducts(
       const errorMessage =
         error.response.data?.message || "Failed to fetch products";
       console.error("Error fetching products:", errorMessage);
-      return { success: false, message: errorMessage };
+      return { success: false, message: errorMessage, products:MockProducts };
     }
 
     console.error("Unexpected error fetching products:", error);
-    return { success: false, message: "An unexpected error occurred" };
+    return {
+      success: false,
+      message: "An unexpected error occurred",
+ 
+      products: MockProducts,
+    };
   }
 }
 
